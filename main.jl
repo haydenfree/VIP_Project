44055c6a6b466fc14475344269997ac598819dd0
benbrock19 <benbrock19@gmail.com>
2023-02-09 17:44:35 -0700

    added function random number

--- main.jl
+++ main.jl
@@ -1,7 +1,8 @@
-# Authors: Hayden Free <hayden.free@gatech.edu>
+# Authors: Hayden Free <hayden.free@gatech.edu>, Ben Brock <benbrock19@gmail.com>
 
 function main()
     print_numbers() # Call print nums to print numbers 1-10
+    random_number() # Call Random number from 1-10
 end
 
 function print_numbers() # define print numbers function
@@ -9,6 +10,11 @@ function print_numbers() # define print numbers function
     for num in nums
         println(num)
     end
+end
+
+function random_number()
+    number = rand(1:10)
+    println(number)
 end
 
 main() # Call main function
